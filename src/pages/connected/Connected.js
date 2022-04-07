d70d36bba159cc5a03f75df4b28830ddd3151130
Quinn Patterson <quinnpatterson5524@gmail.com>
2022-04-07 18:26:08 -0500

    Updated the Connected.js file to working condition

--- src/pages/connected/Connected.js
+++ src/pages/connected/Connected.js
@@ -1,7 +1,34 @@
-const Connected = () =>{
-    return(
-        <p>Connected</p>
-    );
-};
+import FullLayOut from "../../components/Base/FullLayOut";
+import ConnectedResult from "../../components/Results/ConnectedResult";
+import { Modal, Button } from "antd";
+import { useEffect, useState } from "react";
+import MainStats from "../../components/Stats/MainStats";
+import { useDispatch, useSelector } from "react-redux";
+import { navBarActions } from "../../store/store";
+const Connected = () => {
+  const modal = useSelector((state)=>state.navbar.modal)
+  const dispatch = useDispatch()
 
+  return (
+    <div>
+      <Modal
+        style={{ marginLeft: "38%" }}
+        title="Node Connection Staus"
+        visible={modal}
+        onOk={()=>dispatch(navBarActions.setModal(false))}
+        onCancel={()=>dispatch(navBarActions.setModal(false))}
+        footer={[
+          <Button key="submit" type="primary" onClick={()=>dispatch(navBarActions.setModal(false))}>
+            Sounds Good!
+          </Button>,
+        ]}
+      >
+        <ConnectedResult />
+      </Modal>
+      <FullLayOut titleone="Limbo"titletwo="Node">
+        <MainStats />
+      </FullLayOut>
+    </div>
+  );
+};
 export default Connected;
